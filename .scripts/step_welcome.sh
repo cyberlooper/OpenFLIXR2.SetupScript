1a16dd28b76eac9a3b39ab74746d599bb6666430
MagicalCodeMonkey <mwoodru@gmail.com>
2019-03-02 17:06:55 -0500

    Updates to welcome step

--- .scripts/step_welcome.sh
+++ .scripts/step_welcome.sh
@@ -9,25 +9,14 @@ step_welcome() {
     HEIGHT=30
     WIDTH=75
 
-read -r -d '' message << EOM
-Welcome to OpenFLIXR setup!
-This will guide you through getting your OpenFLIXR box setup (mostly).
-Once completed, you will need to configure the applications appropriately.
-
-As you go through this, use the following to navaigate:
-[up]/[down] to move between options
-[space] to select an option
-[tab] to move to buttons or back to the options
-[page up]/[page down] to scroll
-
-Good luck!
-EOM
-
     # Dialog to display
     dialog \
         --backtitle "OpenFLIXR Setup" \
         --title "Step ${step_number}: ${step_name}" \
         --clear \
-        --msgbox "${message}" \
+        --msgbox "$(cat ${SCRIPTPATH}/.misc/welcome.txt)" \
         $HEIGHT $WIDTH
+
+    HEIGHT=$HEIGHT_ORIGINAL
+    WIDTH=$WIDTH_ORIGINAL
 }
