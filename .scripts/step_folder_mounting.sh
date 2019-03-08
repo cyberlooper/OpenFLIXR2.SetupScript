874845c1f9b029b3a3def6f90413555df8fdd348
MagicalCodeMonkey <mwoodru@gmail.com>
2019-03-08 16:29:45 -0500

    Update folder mounting text

--- .scripts/step_folder_mounting.sh
+++ .scripts/step_folder_mounting.sh
@@ -4,7 +4,7 @@ IFS=$'\n\t'
 
 step_folder_mounting() {
     MOUNT_MANAGE="webmin"
-    whiptail --title "Step ${step_number}: ${step_name}" --msgbox "Visit webmin to complete the setup of your folders. http://${LOCAL_IP}/webmin/" 10 75
+    whiptail --title "Step ${step_number}: ${step_name}" --msgbox "$(cat ${SCRIPTPATH}/.misc/folder_mounting.txt)\nhttp://${LOCAL_IP}/webmin/" 10 75
     run_script 'check_response' $?;
 
     set_config "MOUNT_MANAGE" $MOUNT_MANAGE
