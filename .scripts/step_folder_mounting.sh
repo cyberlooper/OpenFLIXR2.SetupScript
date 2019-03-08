b13c395ceef3dc4cfe22cd8be7346d1b2edcefa2
MagicalCodeMonkey <mwoodru@gmail.com>
2019-03-08 16:36:00 -0500

    Updates to folder_mounting dialog

--- .scripts/step_folder_mounting.sh
+++ .scripts/step_folder_mounting.sh
@@ -4,7 +4,8 @@ IFS=$'\n\t'
 
 step_folder_mounting() {
     MOUNT_MANAGE="webmin"
-    whiptail --title "Step ${step_number}: ${step_name}" --msgbox "$(cat ${SCRIPTPATH}/.misc/folder_mounting.txt)\nhttp://${LOCAL_IP}/webmin/" 10 75
+    whiptail --title "Step ${step_number}: ${step_name}" --msgbox "$(cat ${SCRIPTPATH}/.misc/folder_mounting.txt)" 10 75
+    whiptail --title "Step ${step_number}: ${step_name}" --msgbox "Visit webmin to complete the mounting of your folders. Webmin can be found at http://${LOCAL_IP}/webmin/" 10 75
     run_script 'check_response' $?;
 
     set_config "MOUNT_MANAGE" $MOUNT_MANAGE
