5060676d8eaffb758d2c3216a1b1aa0b9fb83380
MagicalCodeMonkey <mwoodru@gmail.com>
2019-03-08 18:26:45 -0500

    run pip using sudo

--- .scripts/install_tzupdate.sh
+++ .scripts/install_tzupdate.sh
@@ -4,5 +4,5 @@ IFS=$'\n\t'
 
 install_tzupdate() {
     info "Installing latest tzupdate."
-    pip install -U tzupdate || fatal "Failed to install tzupdate from pip."
+    sudo pip install -U tzupdate || fatal "Failed to install tzupdate from pip."
 }
