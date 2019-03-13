7361e6e98d7d5126a09aef0ec243de8b26df2a37
MagicalCodeMonkey <mwoodru@gmail.com>
2019-03-13 16:28:01 -0400

    Update wait step

--- .scripts/step_wait.sh
+++ .scripts/step_wait.sh
@@ -6,28 +6,26 @@ step_wait() {
     {
         local OPENFLIXR_LOGFILE
         OPENFLIXR_LOGFILE="/var/log/updateof.log"
-        local LOG_LINE
-        LOG_LINE=""
+        local UPDATEOF_LINE
+        UPDATEOF_LINE=""
         local start
         start=$(date +%s)
-        while [[ ! $LOG_LINE = "Set Version" ]]; do
-            tail -5 $OPENFLIXR_LOGFILE > "${SCRIPTPATH}/tmp.log"
-            while IFS='' read -r line || [[ -n "$line" ]]; do
-                LOG_LINE="$line"
-
-                if [[ $LOG_LINE = "Set Version" ]]; then
-                    info "Found 'Set Version' in $OPENFLIXR_LOGFILE"
-                    break
-                fi
-            done < "${SCRIPTPATH}/tmp.log"
-            sleep 5s
-
+        while [[ ! $UPDATEOF_LINE = "updateof finished" ]]; do
             local elapsed
             elapsed=$(($(date +%s)-$start))
             local duration
             duration=$(date -ud @$elapsed +'%M minutes %S seconds')
             local percent
             percent=$(($elapsed/10))
+
+            UPDATEOF_LINE=$(tail -1 $OPENFLIXR_LOGFILE)
+
+            if [[ $UPDATEOF_LINE = "updateof finished" ]]; then
+                info "Found 'updateof finished' in $OPENFLIXR_LOGFILE"
+                info "Waited for ${duration}"
+                break
+            fi
+            sleep 5s
 
             if [[ $(date -ud @$elapsed +%M) -ge 16 ]]; then
                 echo -e "XXX\n100\Failure!\nXXX"
@@ -42,7 +40,6 @@ step_wait() {
                 echo -e "XXX\n$percent\nDuration: $duration\nXXX"
             fi
         done
-        rm "${SCRIPTPATH}/tmp.log"
         echo -e "XXX\n100\nDone!\nXXX"
     } | whiptail --title "Step ${step_number}: Checking to make sure OpenFLIXR is ready." --gauge "This may take about 15 minutes depending on when you ran this script..." 10 75 0
 
