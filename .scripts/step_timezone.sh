ab8fc1a351a35a1d6b16a349be6aec5387eeb614
MagicalCodeMonkey <mwoodru@gmail.com>
2019-03-13 22:57:25 -0400

    Change to new variable

--- .scripts/step_timezone.sh
+++ .scripts/step_timezone.sh
@@ -6,13 +6,13 @@ step_timezone() {
     # Run timezone selector
     run_script 'tzSelectionMenu' "OpenFLIXR Setup"
 
-    if [ $selected = 0 ]; then
+    if [ "$TZ_CORRECT" = "Y" ]; then
         set_config "OPENFLIXR_TIMEZONE" $detected
         set_config "OPENFLIXR_TIMEZONE_SHORT" $detected_short
         info "Timezone detected: $detected_short"
     else
         set_config "OPENFLIXR_TIMEZONE" $selected
         set_config "OPENFLIXR_TIMEZONE_SHORT" $selected_short
-        info "Timezone detected: $selected_short"
+        info "Timezone selected: $selected_short"
     fi
 }
