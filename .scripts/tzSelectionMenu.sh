081290e4b4e35c2d3a944bc9c4ecef56ecad53e7
MagicalCodeMonkey <mwoodru@gmail.com>
2019-03-13 22:31:16 -0400

    Updates to timezone selection

--- .scripts/tzSelectionMenu.sh
+++ .scripts/tzSelectionMenu.sh
@@ -26,16 +26,15 @@ tzSelectionMenu() {
         if [ -f "/usr/share/zoneinfo/$detected" ]; then
             offset=$(TZ="$detected" date +%z | sed "s/00$/:00/g")
 
-            whiptail --title "Step ${step_number}: ${step_name}" \
-                    --backtitle "$1" \
-                    --yes-button "Yes, correct" \
-                    --no-button "No, I'll choose it" \
-                    --yesno "Your timezone was detected as $detected ($offset). Is it correct?" 0 0
-            selected=$?
-            detected_short=$detected
-            detected="/usr/share/zoneinfo/$detected"
+            if run_script 'question_prompt' N "Your timezone was detected as $detected ($offset). Is it correct?" "Step ${step_number}: ${step_name}" "$1"; then
+                TZ_CORRECT="Y"
+            else
+                TZ_CORRECT="N"
+            fi
 
-            if [ "$selected" = "0" ]; then
+            if [ "$TZ_CORRECT" = "Y" ]; then
+                detected_short=$detected
+                detected="/usr/share/zoneinfo/$detected"
                 tzupdate > /dev/null
                 return
             fi
@@ -45,34 +44,34 @@ tzSelectionMenu() {
     tzRegions
     regionsArray=()
     while read name; do
-        regionsArray+=($name "")
+        regionsArray+=("$name" "")
     done <<< "$regions"
 
-    region=$(whiptail --stdout \
-                      --title "Step ${step_number}: ${step_name}" \
+    # TODO: Figure out why whiptail doesn't like this
+    region=$(whiptail --title "Step ${step_number}: ${step_name}" \
                       --backtitle "$1" \
                       --ok-button "Next" \
                       --no-cancel \
                       --menu "Select a continent or ocean from the menu:" \
-                      20 30 30 \
-                      "${regionsArray[@]}")
+                      20 30 10 \
+                      "${regionsArray[@]}" 3>&1 1>&2 2>&3)
+    warning "AFTER REGION"
 
     tzOptionsByRegion $region
 
     optionsArray=()
     while read name; do
         offset=$(TZ="$region/$name" date +%z | sed "s/00$/:00/g")
-        optionsArray+=($name "($offset)")
+        optionsArray+=("$name" "($offset)")
     done <<< "$options"
 
-    tz=$(whiptail --stdout \
-                    --title "Step ${step_number}: ${step_name}" \
+    tz=$(whiptail --title "Step ${step_number}: ${step_name}" \
                     --backtitle "$1" \
                     --ok-button "Next" \
                     --cancel-button "Back to Regions" \
                     --menu "Select your timezone in ${region}:" \
-                    20 40 30 \
-                    "${optionsArray[@]}")
+                    20 40 10 \
+                    "${optionsArray[@]}" 3>&1 1>&2 2>&3)
 
     if [[ -z "${tz// }" ]]; then
         tzSelectionMenu
