e12e9a4b3992c4f62318f6852957ad19865de2ee
MagicalCodeMonkey <mwoodru@gmail.com>
2019-03-21 23:26:10 -0400

    Update to folder creation step

--- .scripts/step_folder_creation.sh
+++ .scripts/step_folder_creation.sh
@@ -5,12 +5,15 @@ IFS=$'\n\t'
 step_folder_creation() {
     {
         for FOLDER in ${OPENFLIXR_FOLDERS[@]}; do
-            info "Creating /mnt/${FOLDER}/"
             mkdir -p /mnt/${FOLDER}/ > /dev/null
             #TODO: chown openflixr
         done
         echo -e "XXX\n100\nFolders created!\nXXX"
         sleep 2s
     } | whiptail --title "Step ${step_number}: ${step_name}" --gauge "Creating folders" 10 75 0
+
+    for FOLDER in ${OPENFLIXR_FOLDERS[@]}; do
+        info "Created /mnt/${FOLDER}/"
+    done
     info "Folders created!"
 }
