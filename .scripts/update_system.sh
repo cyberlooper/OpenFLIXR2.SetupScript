353e7e5af21bd01e7e09bc357d174da10ffb3005
MagicalCodeMonkey <mwoodru@gmail.com>
2019-03-25 17:27:57 -0400

    Fix up and re-enable the update_system in case it is needed

--- .scripts/update_system.sh
+++ .scripts/update_system.sh
@@ -4,8 +4,6 @@ IFS=$'\n\t'
 
 update_system() {
     if [[ -n "$(command -v apt-get)" ]]; then
-        info "Removing old vagrant packages."
-        apt-get -y remove vagrant > /dev/null 2>&1 || true
         info "Updating repositories."
         apt-get -y update > /dev/null 2>&1 || fatal "Failed to get updates from apt."
         if [[ ${CI:-} != true ]] && [[ ${TRAVIS:-} != true ]]; then
@@ -13,9 +11,7 @@ update_system() {
             apt-get -y dist-upgrade > /dev/null 2>&1 || fatal "Failed to upgrade packages from apt."
         fi
         info "Installing dependencies."
-        apt-get -y install apt-transport-https curl git grep python python-pip rsync sed whiptail > /dev/null 2>&1 || fatal "Failed to install dependencies from apt."
-        # https://cryptography.io/en/latest/installation/#building-cryptography-on-linux
-        apt-get -y install build-essential libssl-dev libffi-dev python3-dev > /dev/null 2>&1 || fatal "Failed to install python cryptography dependencies from apt."
+        apt-get -y install apt-transport-https curl git grep sed whiptail jq > /dev/null 2>&1 || fatal "Failed to install dependencies from apt."
         info "Removing unused packages."
         apt-get -y autoremove > /dev/null 2>&1 || fatal "Failed to remove unused packages from apt."
         info "Cleaning up package cache."
