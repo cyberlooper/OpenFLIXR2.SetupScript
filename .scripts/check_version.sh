e691742626f32dd7d0a267f6a59692c843a106b9
MagicalCodeMonkey <mwoodru@gmail.com>
2019-03-26 10:51:21 -0400

    This should make version check ignore permission changes

--- .scripts/check_version.sh
+++ .scripts/check_version.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 IFS=$'\n\t'
 
 check_version() {
-    git fetch > /dev/null
+    git fetch > /dev/null 2>&1
     if [[ ! -v DEVMODE ]]; then
         readonly GH_COMMIT=$(git rev-parse --short origin/master)
         if [[ "${LOCAL_COMMIT}" != "${GH_COMMIT}" ]]; then
@@ -13,7 +13,7 @@ check_version() {
             exit 0
         fi
     fi
-    GIT_DIFF=$(git diff origin/master -- | cut -c1-5)
+    GIT_DIFF=$(git diff -G. origin/master -- | cut -c1-5)
     if [[ "$GIT_DIFF" != "" ]]; then
         warning "OpenFLIXR Setup Script doesn't match with the master repository."
         if run_script 'question_prompt' Y "OpenFLIXR Setup Script doesn't match with the master repository. Do you want to continue?" "Continue?" ${OF_BACKTITLE}; then
