bad2604a7c22b2e84aaf34d6baaed9fb926018e2
MagicalCodeMonkey <mwoodru@gmail.com>
2019-03-26 18:38:46 -0400

    Fix symlink path issues

--- .scripts/symlink_setupopenflixr.sh
+++ .scripts/symlink_setupopenflixr.sh
@@ -4,8 +4,9 @@ IFS=$'\n\t'
 
 symlink_setupopenflixr() {
     local CHANGED=0
+    local LINK_PATH="/opt/OpenFLIXR2.SetupScript/main.sh"
     # /usr/bin/setupopenflixr
-    if [[ -L "/usr/bin/setupopenflixr" ]] && [[ ${SCRIPTNAME} != "$(readlink -f /usr/bin/setupopenflixr)" ]]; then
+    if [[ -L "/usr/bin/setupopenflixr" ]] && [[ ${LINK_PATH} != "$(readlink -f /usr/bin/setupopenflixr)" ]]; then
         info "Attempting to remove /usr/bin/setupopenflixr symlink."
         rm "/usr/bin/setupopenflixr" || fatal "Failed to remove /usr/bin/setupopenflixr"
     fi
@@ -15,13 +16,13 @@ symlink_setupopenflixr() {
             rm "/usr/bin/setupopenflixr" || fatal "Failed to remove /usr/bin/setupopenflixr"
         fi
         info "Creating /usr/bin/setupopenflixr symbolic link for OpenFLIXR Setup."
-        ln -s -T "${SCRIPTNAME}" /usr/bin/setupopenflixr || fatal "Failed to create /usr/bin/setupopenflixr symlink."
-        chmod +x "${SCRIPTNAME}" > /dev/null 2>&1 || fatal "setupopenflixr must be executable."
+        ln -s -T "${LINK_PATH}" /usr/bin/setupopenflixr || fatal "Failed to create /usr/bin/setupopenflixr symlink."
+        chmod +x "${LINK_PATH}" > /dev/null 2>&1 || fatal "setupopenflixr must be executable."
         CHANGED=1
     fi
 
     # /usr/local/bin/setupopenflixr
-    if [[ -L "/usr/local/bin/setupopenflixr" ]] && [[ ${SCRIPTNAME} != "$(readlink -f /usr/local/bin/setupopenflixr)" ]]; then
+    if [[ -L "/usr/local/bin/setupopenflixr" ]] && [[ ${LINK_PATH} != "$(readlink -f /usr/local/bin/setupopenflixr)" ]]; then
         info "Attempting to remove /usr/local/bin/setupopenflixr symlink."
         rm "/usr/local/bin/setupopenflixr" || fatal "Failed to remove /usr/local/bin/setupopenflixr"
     fi
@@ -31,8 +32,8 @@ symlink_setupopenflixr() {
             rm "/usr/local/bin/setupopenflixr" || fatal "Failed to remove /usr/local/bin/setupopenflixr"
         fi
         info "Creating /usr/local/bin/setupopenflixr symbolic link for OpenFLIXR Setup."
-        ln -s -T "${SCRIPTNAME}" /usr/local/bin/setupopenflixr || fatal "Failed to create /usr/local/bin/setupopenflixr symlink."
-        chmod +x "${SCRIPTNAME}" > /dev/null 2>&1 || fatal "setupopenflixr must be executable."
+        ln -s -T "${LINK_PATH}" /usr/local/bin/setupopenflixr || fatal "Failed to create /usr/local/bin/setupopenflixr symlink."
+        chmod +x "${LINK_PATH}" > /dev/null 2>&1 || fatal "setupopenflixr must be executable."
         CHANGED=1
     fi
     if [[ $CHANGED == 1 ]]; then
