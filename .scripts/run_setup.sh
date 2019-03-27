045e78f38f83fae10f7a085149c5c864bc93068b
MagicalCodeMonkey <mwoodru@gmail.com>
2019-03-27 19:19:44 -0400

    Add new scripts

--- .scripts/run_setup.sh
+++ .scripts/run_setup.sh
@@ -30,7 +30,18 @@ run_setup()
 {
     run_script 'setup_stop_services'
     run_script 'setup_generate_api_keys'
-    run_script 'setup_update_api_keys'
+    run_script 'setup_configure_ombi'
+    run_script 'setup_configure_htpc_manager'
+    run_script 'setup_configure_movie_manager'
+    run_script 'setup_configure_series_manager'
+    run_script 'setup_configure_music_manager'
+    run_script 'setup_configure_comic_manager'
+    run_script 'setup_configure_nzb_downloader'
+    run_script 'setup_configure_torrent_downloader'
+    run_script 'setup_configure_tautulli'
+    run_script 'setup_configure_apps' # TODO: Move what is in here to their proper config scripts
+    run_script 'setup_configure_nginx'
+    run_script 'setup_configure_letsencrypt'
+    run_script 'setup_configure_pihole'
     run_script 'setup_configure_network'
-    run_script 'setup_configure_apps'
 }
