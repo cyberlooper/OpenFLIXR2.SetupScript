6dd4657dfad6238e21eb2a8cfbed7aa4257b13a5
MagicalCodeMonkey <mwoodru@gmail.com>
2019-03-27 19:16:44 -0400

    Add script to configure pihole

---
+++ .scripts/setup_configure_pihole.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+setup_configure_pihole()
+{
+    info "Updating PiHole"
+    if [[ "${config[NETWORK]}" == "static" ]]; then
+        sed -i 's/IPV4_ADDRESS.*/IPV4_ADDRESS='$ip'/' /etc/pihole/setupVars.conf
+        service pihole-FTL restart
+        pihole -g -sd
+    else
+        ip=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
+        sed -i 's/IPV4_ADDRESS.*/IPV4_ADDRESS=/' /etc/pihole/setupVars.conf
+        service pihole-FTL restart
+        pihole -g -sd
+    fi
+
+}
