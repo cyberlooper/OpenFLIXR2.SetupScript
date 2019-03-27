e487589d9aaed6abb47b139d0f939180be39f14b
MagicalCodeMonkey <mwoodru@gmail.com>
2019-03-27 19:12:49 -0400

    Move code into other script files

--- .scripts/step_setup.sh
+++ .scripts/step_setup.sh
@@ -6,21 +6,7 @@ step_setup() {
     info 'Preparing for setup...'
     # Set setup variables
     # TODO: Move/rename these at some point
-    networkconfig=${config[NETWORK]}
-    ip=${config[OPENFLIXR_IP]}
-    subnet=${config[OPENFLIXR_SUBNET]}
-    gateway=${config[OPENFLIXR_GATEWAY]}
-    dns='127.0.0.1'
     password="${OPENFLIXIR_PASSWORD:-}"
-    if [[ ${config[ACCESS]} = 'remote' ]]; then
-        letsencrypt='on'
-        domainname=${config[LETSENCRYPT_DOMAIN]}
-        email=${config[LETSENCRYPT_EMAIL]}
-    else
-        letsencrypt='off'
-        domainname=''
-        email=''
-    fi
     oldpassword=""
     if [[ -f "/usr/share/nginx/html/setup/config.ini" ]]; then
         oldpassword=$(crudini --get /usr/share/nginx/html/setup/config.ini password oldpassword)
