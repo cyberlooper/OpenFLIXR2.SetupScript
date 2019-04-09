41d1450e9621d33ee1420afab4370a19b0ff5c47
MagicalCodeMonkey <mwoodru@gmail.com>
2019-04-09 17:06:43 -0400

    Updates to timezone step

--- .scripts/step_timezone.sh
+++ .scripts/step_timezone.sh
@@ -3,30 +3,45 @@ set -euo pipefail
 IFS=$'\n\t'
 
 step_timezone() {
-    # Add domains that tzupdate uses for timezone lookup to pi-hole
-    info "Adding some domains to pi-hole for timezone detection"
-    pihole -w ip-api.com > /dev/null
-    pihole -w freegeoip.app > /dev/null
-    pihole -w geoip.nekudo.com > /dev/null
-    pihole -w timezoneapi.io > /dev/null
-
-    # Run timezone selector
-    run_script 'tzSelectionMenu' ${OF_BACKTITLE}
-
-    if [ "$TZ_CORRECT" = "Y" ]; then
-        set_config "OPENFLIXR_TIMEZONE" $detected
-        set_config "OPENFLIXR_TIMEZONE_SHORT" $detected_short
-        info "Timezone detected: $detected_short"
+    if [[ "${config[OPENFLIXR_TIMEZONE_SHORT]}" == "" ]]; then
+        log "Timezone not set."
+        CHANGE_TZ="Y"
+    elif run_script 'question_prompt' N "Your current timezone is set to ${config[OPENFLIXR_TIMEZONE_SHORT]}.\nDo you want to change it?" "Step ${step_number}: ${step_name}"; then
+        log "Timezone set: ${config[OPENFLIXR_TIMEZONE_SHORT]}. User chose to change the timezone."
+        CHANGE_TZ="Y"
     else
-        set_config "OPENFLIXR_TIMEZONE" $selected
-        set_config "OPENFLIXR_TIMEZONE_SHORT" $selected_short
-        info "Timezone selected: $selected_short"
+        log "Timezone set: ${config[OPENFLIXR_TIMEZONE_SHORT]}. User chose to keep the timezone."
+        CHANGE_TZ="N"
     fi
 
-    # Remove domains that tzupdate uses for timezone lookup to pihole
-    info "Removing some domains to pi-hole for timezone detection"
-    pihole -w -d ip-api.com > /dev/null
-    pihole -w -d freegeoip.app > /dev/null
-    pihole -w -d geoip.nekudo.com > /dev/null
-    pihole -w -d timezoneapi.io > /dev/null
+    if [[ "$CHANGE_TZ" == "Y" ]]; then
+        # Add domains that tzupdate uses for timezone lookup to pi-hole
+        info "Adding some domains to pi-hole for timezone detection"
+        pihole -w ip-api.com > /dev/null
+        pihole -w freegeoip.app > /dev/null
+        pihole -w geoip.nekudo.com > /dev/null
+        pihole -w timezoneapi.io > /dev/null
+
+        # Run timezone selector
+        run_script 'tzSelectionMenu' ${OF_BACKTITLE}
+
+        if [ "$TZ_CORRECT" = "Y" ]; then
+            set_config "OPENFLIXR_TIMEZONE" $detected
+            set_config "OPENFLIXR_TIMEZONE_SHORT" $detected_short
+            info "Timezone detected: $detected_short"
+        else
+            set_config "OPENFLIXR_TIMEZONE" $selected
+            set_config "OPENFLIXR_TIMEZONE_SHORT" $selected_short
+            info "Timezone selected: $selected_short"
+        fi
+
+        # Remove domains that tzupdate uses for timezone lookup to pihole
+        info "Removing some domains to pi-hole for timezone detection"
+        pihole -w -d ip-api.com > /dev/null
+        pihole -w -d freegeoip.app > /dev/null
+        pihole -w -d geoip.nekudo.com > /dev/null
+        pihole -w -d timezoneapi.io > /dev/null
+    else
+        log "Keeping current timezone setting."
+    fi
 }
