a80a57ae6ef57f76f99752a011edee1762c3cefe
MagicalCodeMonkey <mwoodru@gmail.com>
2019-04-20 10:03:39 -0400

    Add output to configure let's encrypt

--- .scripts/setup_configure_letsencrypt.sh
+++ .scripts/setup_configure_letsencrypt.sh
@@ -6,6 +6,7 @@ setup_configure_letsencrypt()
 {
     if [[ "${config[LETSENCRYPT]}" == "on" ]]; then
         info "Configuring Let's Encrypt"
-        sudo bash /opt/openflixr/letsencrypt.sh
+        bash /opt/openflixr/letsencrypt.sh
+        info "Configured!"
     fi
 }
