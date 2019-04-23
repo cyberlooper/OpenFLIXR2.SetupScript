8c66f01c51b29006214af97927ee2f262b83410a
MagicalCodeMonkey <mwoodru@gmail.com>
2019-04-23 14:39:50 -0400

    Add a script for misc fixes to the setup

---
+++ .scripts/setup_fixes.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+setup_fixes()
+{
+    info "Various fixes not handled anywhere else in setup"
+    info "  Sonarr apt repo fix"
+    apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 0xA236C58F409091A18ACA53CBEBFF6B99D9B78493 > /dev/null
+    if [[ -f "/etc/apt/sources.list.d/sonarr.list" ]]; then
+        rm /etc/apt/sources.list.d/sonarr.list
+    fi
+    echo "deb http://apt.sonarr.tv/ master main" | tee /etc/apt/sources.list.d/sonarr.list > /dev/null
+    apt update > /dev/null
+}
