531b34d6c4ea976e41f60d44a3add947e2fd3092
MagicalCodeMonkey <mwoodru@gmail.com>
2019-04-22 22:46:22 -0400

    Make Let's Encrypt no longer bomb out setup - Collect logs on failure - Test nginx and collect logs if failed as well

--- .scripts/setup_configure_letsencrypt.sh
+++ .scripts/setup_configure_letsencrypt.sh
@@ -6,7 +6,26 @@ setup_configure_letsencrypt()
 {
     if [[ "${config[LETSENCRYPT]}" == "on" ]]; then
         info "Configuring Let's Encrypt"
-        bash /opt/openflixr/letsencrypt.sh
-        info "Configured!"
+        bash /opt/openflixr/letsencrypt.sh || LETSENCRYPT_STATUS="FAILED"
+        if [[ ${LETSENCRYPT_STATUS:-} == "FAILED" ]]; then
+            warning "Let's Encrypt configuration failed! =("
+            warning "Usually this can be fixed later, so we won't kill the setup but will collect some information."
+            info "Adding Let's Encrypt logs to setup logs for troubleshooting"
+            LE_LOGS_START=$(($(grep -n "Run pre hook" /var/log/letsencrypt.log | tail -1 | awk 'BEGIN{FS=":"}{print $(1)}')-1))
+            LE_LOGS_END=$(wc -l /var/log/letsencrypt.log | awk '{print $1}')
+            TAIL_COUNT=$(($LE_LOGS_END-$LE_LOGS_START))
+            debug "TAIL_COUNT=${TAIL_COUNT}"
+            tail -$TAIL_COUNT /var/log/letsencrypt.log >> $LOG_FILE
+            info "Checking nginx conf"
+            if [[ $(sudo nginx -t 2>&1 | grep -c "failed") != 0 ]]; then
+                warning "nginx conf test failed"
+                nginx -t >> $LOG_FILE
+                warning "nginx cannot run =("
+            else
+                info "- nginx can still run!"
+            fi
+        else
+            info "Configured!"
+        fi
     fi
 }
