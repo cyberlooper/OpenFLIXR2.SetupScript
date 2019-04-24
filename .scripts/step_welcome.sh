3d51f0396ccb7d5184bd3b2010e5bd850938acb8
MagicalCodeMonkey <mwoodru@gmail.com>
2019-04-24 18:10:59 -0400

    Cleaning up some variables

--- .scripts/step_welcome.sh
+++ .scripts/step_welcome.sh
@@ -3,15 +3,11 @@ set -euo pipefail
 IFS=$'\n\t'
 
 step_welcome() {
-    # Variables
-    local HEIGHT=30
-    local WIDTH=75
-
     # Dialog to display
     whiptail \
         --backtitle ${OF_BACKTITLE} \
         --title "Step ${step_number}: ${step_name}" \
         --clear \
         --msgbox "$(cat ${SCRIPTPATH}/.misc/welcome.txt)" \
-        ${HEIGHT:-0} ${WIDTH:-0}
+        0 0
 }
