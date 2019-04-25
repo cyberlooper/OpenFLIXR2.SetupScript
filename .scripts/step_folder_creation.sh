6cde5a64d07fe6ee79a68a865a73bd398c2a92e1
MagicalCodeMonkey <mwoodru@gmail.com>
2019-04-25 08:54:45 -0400

    Move folder creation and update it's code

--- .scripts/step_folder_creation.sh
+++ .scripts/step_folder_creation.sh
@@ -3,19 +3,14 @@ set -euo pipefail
 IFS=$'\n\t'
 
 step_folder_creation() {
-    {
-        for FOLDER in ${OPENFLIXR_FOLDERS[@]}; do
-            mkdir -p /mnt/${FOLDER}/ > /dev/null
-        done
-        echo -e "XXX\n100\nFolders created!\nXXX"
-        sleep 2s
-    } | whiptail --title "Step ${step_number}: ${step_name}" --gauge "Creating folders" 10 75 0
-
+    info "Creating /mnt folders"
     for FOLDER in ${OPENFLIXR_FOLDERS[@]}; do
-        info "Created /mnt/${FOLDER}/"
+        mkdir -p /mnt/${FOLDER}/ >> ${LOG_FILE}
+        info "- Created /mnt/${FOLDER}/"
+        sleep 2s
     done
-    info "Folders created!"
-    info "Updating folder permissions..."
-    chown openflixr:openflixr -R /mnt
-    chmod g+w -R /mnt
+    info "- Folders created!"
+    info "- Updating folder permissions..."
+    chown openflixr:openflixr -R /mnt || warning "  Unable to change ownership of /mnt"
+    chmod g+w -R /mnt >> ${LOG_FILE} || warning "  Unable to change permissions of /mnt"
 }
