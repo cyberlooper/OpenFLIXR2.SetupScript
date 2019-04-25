9fcb74774580e3e9fa4dde874a7e5c77ed136118
MagicalCodeMonkey <mwoodru@gmail.com>
2019-04-25 08:55:28 -0400

    Fix the fixes; add nginx fix, just in case

--- .scripts/setup_fixes.sh
+++ .scripts/setup_fixes.sh
@@ -5,7 +5,7 @@ IFS=$'\n\t'
 setup_fixes()
 {
     info "Various fixes not handled anywhere else in setup"
-    info "  Sonarr apt repo fix"
+    info "- Sonarr apt repo fix"
     apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 0xA236C58F409091A18ACA53CBEBFF6B99D9B78493 > /dev/null 2>&1
     if [[ -f "/etc/apt/sources.list.d/sonarr.list" ]]; then
         rm /etc/apt/sources.list.d/sonarr.list
@@ -13,11 +13,17 @@ setup_fixes()
     echo "deb http://apt.sonarr.tv/ master main" | tee /etc/apt/sources.list.d/sonarr.list > /dev/null
     apt-get update > /dev/null
 
-    info "  Permissions fixes"
+    info "- Permissions fixes"
     # Add root to openflixr group
-    usermod -a -G openflixr root
+    usermod -a -G openflixr root || warning "  Unable to add 'root' user to 'openflixr' group"
     # Make /mnt group be openflixr
-    chown openflixr:openflixr -R /mnt
+    chown openflixr:openflixr -R /mnt || warning "  Unable to change ownership of /mnt"
     # Add group write permissions to /mnt
-    chmod g+w -R /mnt
+    chmod g+w -R /mnt >> ${LOG_FILE} || warning "  Unable to change permissions of /mnt"
+
+    if [[ -f "/etc/nginx/sites-enabled/reverse" ]]; then
+        info "- Moving old nginx setting file"
+        mv /etc/nginx/sites-enabled/reverse /opt/openflixr || warning "  Unable to move file"
+        service nginx restart
+    fi
 }
