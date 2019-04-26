ef79dedc4090f7040c3e3f3a38675460dacf83eb
MagicalCodeMonkey <mwoodru@gmail.com>
2019-04-26 00:14:23 -0400

    Change how Ombi's API key is set

--- .scripts/setup_configure_ombi.sh
+++ .scripts/setup_configure_ombi.sh
@@ -5,5 +5,16 @@ IFS=$'\n\t'
 setup_configure_ombi()
 {
     info "Configuring Ombi"
+    info "- Setting API Key"
+    local ombi_settings
+    ombi_settings=$(sqlite3 /opt/Ombi/OmbiSettings.db "SELECT Content FROM GlobalSettings WHERE SettingsName='OmbiSettings'")
+    # Set Ombi API Key
+    debug "  Setting API Key to: ${API_KEYS[ombi]}"
+    ombi_settings=$(sed 's/"ApiKey":".*","IgnoreCertificateErrors"/"ApiKey":"'${API_KEYS[ombi]}'","IgnoreCertificateErrors"/' <<< $ombi_settings)
+    debug "  Updating DB"
+    sqlite3 /opt/Ombi/OmbiSettings.db "UPDATE GlobalSettings SET Content='$ombi_settings' WHERE SettingsName='OmbiSettings'"
+    info "Restarting Ombi"
+    service ombi restart
+    sleep 10s
     run_script 'setup_configure_ombi_password'
 }
