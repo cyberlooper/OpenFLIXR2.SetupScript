558e617dc1d61751a0ad5ef74336bdfefe1be332
MagicalCodeMonkey <mwoodru@gmail.com>
2019-04-29 12:26:24 -0400

    update output to log

--- .scripts/setup_configure_nginx_password.sh
+++ .scripts/setup_configure_nginx_password.sh
@@ -6,8 +6,8 @@ setup_configure_nginx_password()
 {
     if [[ ${config[CHANGE_PASS]} == "Y" && ${OPENFLIXR_PASSWORD_NEW} != "" ]]; then
         info "- Updating Password"
-        echo openflixr:"${OPENFLIXR_PASSWORD_NEW}" | sudo chpasswd
-        htpasswd -b /etc/nginx/.htpasswd openflixr "${OPENFLIXR_PASSWORD_NEW}"
-        crudini --set /usr/share/nginx/html/setup/config.ini password oldpassword ${OPENFLIXR_PASSWORD_NEW}
+        echo openflixr:"${OPENFLIXR_PASSWORD_NEW}" | sudo chpasswd >> $LOG_FILE 2>&1
+        htpasswd -b /etc/nginx/.htpasswd openflixr "${OPENFLIXR_PASSWORD_NEW}" >> $LOG_FILE 2>&1
+        crudini --set /usr/share/nginx/html/setup/config.ini password oldpassword ${OPENFLIXR_PASSWORD_NEW} >> $LOG_FILE 2>&1
     fi
 }
