96db09fc6e2a7b9a6b5172e072c31d8ac57f4501
MagicalCodeMonkey <mwoodru@gmail.com>
2019-04-29 14:42:34 -0400

    Add openflixr user to Ombi if doesn't already exist

--- .scripts/setup_configure_ombi.sh
+++ .scripts/setup_configure_ombi.sh
@@ -11,8 +11,60 @@ setup_configure_ombi()
     # Set Ombi API Key
     debug "  Setting API Key to: ${API_KEYS[ombi]}"
     ombi_settings=$(sed 's/"ApiKey":".*","IgnoreCertificateErrors"/"ApiKey":"'${API_KEYS[ombi]}'","IgnoreCertificateErrors"/' <<< $ombi_settings)
-    info "- Updating DB"
+    info "- Updating DB with the API Key"
     sqlite3 /opt/Ombi/OmbiSettings.db "UPDATE GlobalSettings SET Content='$ombi_settings' WHERE SettingsName='OmbiSettings'"
+    info "- Checking for the openflixr user"
+    ombi_openflixr=$(sqlite3 /opt/Ombi/Ombi.db "SELECT COUNT(Id) FROM AspNetUsers WHERE NormalizedUserName='OPENFLIXR';")
+    if [[ $ombi_openflixr = 1 ]]; then
+        info "  Found!"
+    else
+        info "  Not found... Adding openflixr user"
+        curl -X POST "http://localhost:3579/api/v1/Identity" \
+            -H "accept: application/json" \
+            -H "ApiKey: ${API_KEYS[ombi]}" \
+            -H "Content-Type: application/json-patch+json" \
+            -d "{
+                    \"alias\":\"\",
+                    \"claims\":[
+                        {\"value\":\"ReceivesNewsletter\",\"enabled\":false},
+                        {\"value\":\"RequestMusic\",\"enabled\":false},
+                        {\"value\":\"AutoApproveMusic\",\"enabled\":false},
+                        {\"value\":\"ManageOwnRequests\",\"enabled\":false},
+                        {\"value\":\"EditCustomPage\",\"enabled\":false},
+                        {\"value\":\"AutoApproveMovie\",\"enabled\":false},
+                        {\"value\":\"Admin\",\"enabled\":true},
+                        {\"value\":\"AutoApproveTv\",\"enabled\":false},
+                        {\"value\":\"PowerUser\",\"enabled\":false},
+                        {\"value\":\"RequestMovie\",\"enabled\":false},
+                        {\"value\":\"RequestTv\",\"enabled\":false},
+                        {\"value\":\"Disabled\",\"enabled\":false}
+                    ],
+                    \"emailAddress\":\"\",
+                    \"id\":\"\",
+                    \"password\":\"openflixr\",
+                    \"userName\":\"openflixr\",
+                    \"userType\":1,
+                    \"checked\":false,
+                    \"hasLoggedIn\":false,
+                    \"lastLoggedIn\":\"2019-04-29T18:07:24.250Z\",
+                    \"episodeRequestLimit\":0,
+                    \"movieRequestLimit\":0,
+                    \"userAccessToken\":\"\",
+                    \"musicRequestLimit\":0,
+                    \"episodeRequestQuota\":null,
+                    \"movieRequestQuota\":null,
+                    \"userQualityProfiles\":{
+                        \"radarrQualityProfile\":0,
+                        \"radarrRootPath\":0,
+                        \"sonarrQualityProfile\":0,
+                        \"sonarrQualityProfileAnime\":0,
+                        \"sonarrRootPath\":0,
+                        \"sonarrRootPathAnime\":0
+                    },
+                    \"musicRequestQuota\":null
+                }"
+        info "  Added!"
+    fi
     info "- Restarting Ombi"
     service ombi restart
     sleep 10s
