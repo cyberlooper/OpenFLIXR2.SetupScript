67a10694f98e878068802e80a8efc9e29f3fedb8
MagicalCodeMonkey <mwoodru@gmail.com>
2019-04-29 10:01:53 -0400

    Changed my mind on part of how updating works

--- .scripts/check_version.sh
+++ .scripts/check_version.sh
@@ -5,14 +5,14 @@ IFS=$'\n\t'
 check_version() {
     git fetch > /dev/null 2>&1
     if [[ ! -v DEVMODE ]]; then
-        readonly GH_COMMIT=$(git rev-parse --short origin/${config[BRANCH]})
+        readonly GH_COMMIT=$(git rev-parse --short ${config[BRANCH]})
         if [[ "${LOCAL_COMMIT}" != "${GH_COMMIT}" ]]; then
             warning "OpenFLIXR Setup Script is not up-to-date."
             warning "Please run 'sudo setupopenflixr -u' to get the latest."
             exit 0
         fi
     fi
-    GIT_DIFF=$(git diff -G. origin/${config[BRANCH]} -- | cut -c1-5)
+    GIT_DIFF=$(git diff -G. ${config[BRANCH]} -- | cut -c1-5)
     if [[ "$GIT_DIFF" != "" ]]; then
         warning "OpenFLIXR Setup Script doesn't match with the repository's ${config[BRANCH]} branch."
         if run_script 'question_prompt' Y "OpenFLIXR Setup Script doesn't match with the repository's ${config[BRANCH]} branch. Do you want to continue?" "Continue?" ${OF_BACKTITLE}; then
--- .scripts/update_self.sh
+++ .scripts/update_self.sh
@@ -13,7 +13,7 @@ update_self() {
     fi
     cd "${SCRIPTPATH}" || fatal "Failed to change to ${SCRIPTPATH} directory."
     git fetch > /dev/null 2>&1 || fatal "Failed to fetch recent changes from git."
-    git reset --hard "origin/${BRANCH}" > /dev/null 2>&1 || fatal "Failed to reset to origin/${BRANCH}."
+    git reset --hard "${BRANCH}" > /dev/null 2>&1 || fatal "Failed to reset to ${BRANCH}."
     git pull > /dev/null 2>&1 || fatal "Failed to pull recent changes from git."
     git for-each-ref --format '%(refname:short)' refs/heads | grep -v master | xargs git branch -D > /dev/null 2>&1 || true
     chmod +x "${SCRIPTNAME}" > /dev/null 2>&1 || fatal "OpenFLIXR2 Setup Script must be executable."
