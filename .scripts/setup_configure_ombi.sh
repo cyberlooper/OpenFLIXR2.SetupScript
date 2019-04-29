dfdbd99224a3c4a8827d9aba25c3b79451d5a6a2
MagicalCodeMonkey <mwoodru@gmail.com>
2019-04-28 23:06:11 -0400

    Add Ombi to HTPC

--- .scripts/setup_configure_ombi.sh
+++ .scripts/setup_configure_ombi.sh
@@ -11,10 +11,50 @@ setup_configure_ombi()
     # Set Ombi API Key
     debug "  Setting API Key to: ${API_KEYS[ombi]}"
     ombi_settings=$(sed 's/"ApiKey":".*","IgnoreCertificateErrors"/"ApiKey":"'${API_KEYS[ombi]}'","IgnoreCertificateErrors"/' <<< $ombi_settings)
-    debug "  Updating DB"
+    info "- Updating DB"
     sqlite3 /opt/Ombi/OmbiSettings.db "UPDATE GlobalSettings SET Content='$ombi_settings' WHERE SettingsName='OmbiSettings'"
     info "- Restarting Ombi"
     service ombi restart
     sleep 10s
+
     run_script 'setup_configure_ombi_password'
+
+    log "- Enabling OMBI in HTPC"
+    sqlite3 /opt/HTPCManager/userdata/database.db "INSERT OR REPLACE INTO setting (id, key, val)
+                                                    VALUES (  (SELECT id FROM setting WHERE key='ombi_enable'),
+                                                            'ombi_enable',
+                                                            'on'
+                                                        );"
+    sqlite3 /opt/HTPCManager/userdata/database.db "INSERT OR REPLACE INTO setting (id, key, val)
+                                                    VALUES (  (SELECT id FROM setting WHERE key='ombi_name'),
+                                                            'ombi_name',
+                                                            'Ombi'
+                                                        );"
+    sqlite3 /opt/HTPCManager/userdata/database.db "INSERT OR REPLACE INTO setting (id, key, val)
+                                                    VALUES (  (SELECT id FROM setting WHERE key='ombi_host'),
+                                                            'ombi_host',
+                                                            'localhost'
+                                                        );"
+    sqlite3 /opt/HTPCManager/userdata/database.db "INSERT OR REPLACE INTO setting (id, key, val)
+                                                    VALUES (  (SELECT id FROM setting WHERE key='ombi_port'),
+                                                            'ombi_port',
+                                                            '3579'
+                                                        );"
+    sqlite3 /opt/HTPCManager/userdata/database.db "INSERT OR REPLACE INTO setting (id, key, val)
+                                                    VALUES (  (SELECT id FROM setting WHERE key='ombi_username'),
+                                                            'ombi_username',
+                                                            'openflixr'
+                                                        );"
+    local OMBI_PASSWORD
+    if [[ ${config[CHANGE_PASS]} == "Y" && ${OPENFLIXR_PASSWORD_NEW} != "" ]]; then
+        OMBI_PASSWORD=${OPENFLIXR_PASSWORD_NEW}
+    else
+        OMBI_PASSWORD=${OPENFLIXR_PASSWORD_OLD}
+    fi
+
+    sqlite3 /opt/HTPCManager/userdata/database.db "INSERT OR REPLACE INTO setting (id, key, val)
+                                                    VALUES (  (SELECT id FROM setting WHERE key='ombi_password'),
+                                                            'ombi_password',
+                                                            '${OMBI_PASSWORD}'
+                                                        );"
 }
