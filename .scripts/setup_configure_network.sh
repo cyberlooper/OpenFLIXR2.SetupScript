cde75decb46488889454e7685f723d59d5763df5
MagicalCodeMonkey <mwoodru@gmail.com>
2019-05-07 17:51:27 -0400

    Changing how the interfaces file is built, because why not...

--- .scripts/setup_configure_network.sh
+++ .scripts/setup_configure_network.sh
@@ -4,37 +4,44 @@ IFS=$'\n\t'
 
 setup_configure_network()
 {
+    local NETFILE
+    NETFILE="/etc/network/interfaces"
     ## network
     info "Configuring network..."
 
+    echo "# This file describes the network interfaces available on your system" > "${NETFILE}"
+    echo "# and how to activate them. For more information, see interfaces(5)." >> "${NETFILE}"
+    echo "source /etc/network/interfaces.d/*" >> "${NETFILE}"
+    echo "" >> "${NETFILE}"
+    echo "# The loopback network interface" >> "${NETFILE}"
+    echo "auto lo" >> "${NETFILE}"
+    echo "iface lo inet loopback" >> "${NETFILE}"
+    echo "" >> "${NETFILE}"
+    echo "# The primary network interface" >> "${NETFILE}"
+    echo "auto eth0" >> "${NETFILE}"
+
     if [[ "${config[NETWORK]}" == "static" ]]; then
-        cat > /etc/network/interfaces<<EOF
-# This file describes the network interfaces available on your system
-# and how to activate them. For more information, see interfaces(5).
-source /etc/network/interfaces.d/*
-# The loopback network interface
-auto lo eth0
-iface lo inet loopback
-# The primary network interface
-iface eth0 inet static
-address ${config[OPENFLIXR_IP]}
-netmask ${config[OPENFLIXR_SUBNET]}
-gateway ${config[OPENFLIXR_GATEWAY]}
-dns-nameservers 127.0.0.1
-EOF
+        info "Configuring network to use Static IP"
+
+        echo "iface eth0 inet static" >> "${NETFILE}"
+        echo "    address ${config[OPENFLIXR_IP]}" >> "${NETFILE}"
+        if [[ ${config[OPENFLIXR_SUBNET]} != "" ]]; then
+            echo "    netmask ${config[OPENFLIXR_SUBNET]}" >> "${NETFILE}"
+        fi
+        if [[ ${config[OPENFLIXR_GATEWAY]} != "" ]]; then
+            echo "    gateway ${config[OPENFLIXR_GATEWAY]}" >> "${NETFILE}"
+        fi
+        echo "    dns-nameservers 127.0.0.1" >> "${NETFILE}"
+        echo "" >> "${NETFILE}"
+
         info "Network configured using Static IP"
     else
-        cat > /etc/network/interfaces<<EOF
-# This file describes the network interfaces available on your system
-# and how to activate them. For more information, see interfaces(5).
-source /etc/network/interfaces.d/*
-# The loopback network interface
-auto lo eth0
-iface lo inet loopback
-# The primary network interface
-iface eth0 inet dhcp
-dns-nameservers 127.0.0.1
-EOF
+        info "Configuring network to use DHCP"
+
+        echo "iface eth0 inet dhcp" >> "${NETFILE}"
+        echo "    dns-nameservers 127.0.0.1" >> "${NETFILE}"
+        echo "" >> "${NETFILE}"
+
         info "Network configured using DHCP"
     fi
 }
