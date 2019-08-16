7113114e64f7d26b393c1d281745498a963a73d7
MagicalCodeMonkey <mwoodru@gmail.com>
2019-08-15 20:03:18 -0400

    Update fixes

---
+++ .scripts/setup_fixes_redis.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+setup_fixes_redis()
+{
+    info "Redis fixes"
+    info "- Removing IPv6 binding"
+    sudo sed -i "s/bind 127.0.0.1 ::1/bind 127.0.0.1/g" "/etc/redis/redis.conf"
+    info "- Done"
+}
