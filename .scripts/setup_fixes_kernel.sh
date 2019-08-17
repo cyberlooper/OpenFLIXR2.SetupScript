ad5305b50eb4701fdbdac3641cbc98247535cc70
MagicalCodeMonkey <mwoodru@gmail.com>
2019-08-17 10:22:01 -0400

    Add kernel fix

---
+++ .scripts/setup_fixes_kernel.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+setup_fixes_kernel()
+{
+    info "Kernel fixes"
+    if [[ $(dpkg -l | grep -c linux-generic-hwe-18.04) == 0 ]]; then
+        info "- Installing the HWE kernel"
+        apt-get -y install --install-recommends linux-generic-hwe-18.04
+        warning "- You must reboot your maching after setup completes to get the updated kernel"
+        warning "- Reboot using 'sudo reboot' once this has completed"
+        sleep 10s
+    else
+        info "- Kernel fix already applied and nothing to do!"
+    fi
+    info "- Done"
+}
