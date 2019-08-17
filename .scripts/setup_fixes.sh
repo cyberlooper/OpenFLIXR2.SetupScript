3afd5eb8fbb12d58b28e383f0732a9d0c9b2c325
MagicalCodeMonkey <mwoodru@gmail.com>
2019-08-16 23:35:59 -0400

    Add php7.3-fpm fix

--- .scripts/setup_fixes.sh
+++ .scripts/setup_fixes.sh
@@ -9,6 +9,7 @@ setup_fixes()
     run_script 'setup_fixes_updater'
     run_script 'setup_fixes_mono'
     run_script 'setup_fixes_nginx'
+    run_script 'setup_fixes_php'
     run_script 'setup_fixes_redis'
     run_script 'setup_fixes_sonarr'
     run_script 'setup_fixes_pihole'
