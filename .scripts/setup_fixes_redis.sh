39acff9c7d2c87d5ad72f84c98d872b35489f4c6
MagicalCodeMonkey <mwoodru@gmail.com>
2019-08-17 09:05:44 -0400

    Remove sudo from commands

--- .scripts/setup_fixes_redis.sh
+++ .scripts/setup_fixes_redis.sh
@@ -6,6 +6,6 @@ setup_fixes_redis()
 {
     info "Redis fixes"
     info "- Removing IPv6 binding"
-    sudo sed -i "s/bind 127.0.0.1 ::1/bind 127.0.0.1/g" "/etc/redis/redis.conf"
+    sed -i "s/bind 127.0.0.1 ::1/bind 127.0.0.1/g" "/etc/redis/redis.conf"
     info "- Done"
 }
