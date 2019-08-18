ced28d73e92fd3386c4ec7743b3dd4849644651d
MagicalCodeMonkey <mwoodru@gmail.com>
2019-08-18 13:00:04 -0400

    Updates to mono fixes

--- .scripts/setup_fixes_mono.sh
+++ .scripts/setup_fixes_mono.sh
@@ -12,5 +12,40 @@ setup_fixes_mono()
         info "- Updating mono config"
         mv "/etc/mono/config.dpkg-new" "/etc/mono/config"
     fi
+    info "- Checking sources for Mono"
+    local SOURCE="/etc/apt/sources.list.d/mono-official-stable.list"
+    if [[ ${UBU_VER} != "16.04" ]]; then
+        local REPO="deb https://download.mono-project.com/repo/ubuntu stable-xenial main"
+    elif [[ ${UBU_VER} != "18.04" ]]; then
+        local REPO="deb https://download.mono-project.com/repo/ubuntu stable-bionic main"
+    else
+        return 0
+    fi
+    if [[ ! -f "${SOURCE}"
+        || (-f "${SOURCE}" && $(grep -c "${REPO}" "${SOURCE}") == 0) ]]; then
+        if [[ -f "${SOURCE}" ]]; then
+            info "  - Removing existing Mono sources..."
+            rm ${SOURCE}
+        fi
+        info "  - Adding Mono repo to sources"
+        gpg --list-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF > /dev/null 2>&1 || apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF > /dev/null 2>&1
+        echo "${REPO}" | sudo tee "${SOURCE}" > /dev/null
+        info "  - Updating apt"
+        apt-get -y update > /dev/null 2>&1 || error "  Failed to update apt"
+    else
+        info "  - Nothing to do!"
+    fi
+    info "- Checking if ca-certificates-mono can be installed..."
+    MONO_VERSION=$(apt-cache policy mono-runtime-common | grep "Installed:" | cut -d ':' -f 2 | cut -d '-' -f 1 | cut -d ' ' -f 2)
+    MONO_VERSION_MAJOR=$(cut -d '.' -f 1 <<< $MONO_VERSION)
+    MONO_VERSION_MINOR=$(cut -d '.' -f 2 <<< $MONO_VERSION)
+    if [[ (${MONO_VERSION_MAJOR} -ge 6) || (${MONO_VERSION_MAJOR} -eq 5 && ${MONO_VERSION_MINOR} -ge 20) ]]; then
+        info "  - Installing ca-certificates-mono"
+        apt-get -y install ca-certificates-mono
+    elif [[ ${MONO_VERSION_MAJOR} -le 5 && ${MONO_VERSION_MINOR} -le 20 ]]; then
+        warning "  - Mono needs to be updated before these can be installed."
+    else
+        info "  - Nothing to do!"
+    fi
     info "- Done"
 }
