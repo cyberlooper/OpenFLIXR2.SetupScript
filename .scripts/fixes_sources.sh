6b3e12a915b282c1ee6a056392ffcff57f0b89e3
MagicalCodeMonkey <mwoodru@gmail.com>
2019-08-19 17:48:48 -0400

    Fix an issue with sources fix

--- .scripts/fixes_sources.sh
+++ .scripts/fixes_sources.sh
@@ -5,15 +5,14 @@ IFS=$'\n\t'
 fixes_sources()
 {
     info "Sources fixes"
-    if [[ -f "'/etc/apt/sources.list.d/nijel-ubuntu-phpmyadmin-xenial.list" || -f "/etc/apt/sources.list.d/nijel-ubuntu-phpmyadmin-xenial.list.save" ]]; then
+    if [[ -f "/etc/apt/sources.list.d/nijel-ubuntu-phpmyadmin-xenial.list" || -f "/etc/apt/sources.list.d/nijel-ubuntu-phpmyadmin-xenial.list.save" ]]; then
         info "- Removing bad sources (nijel/phpmyadmin)"
-        if [[ -f "'/etc/apt/sources.list.d/nijel-ubuntu-phpmyadmin-xenial.list" ]]; then
+        if [[ -f "/etc/apt/sources.list.d/nijel-ubuntu-phpmyadmin-xenial.list" ]]; then
             rm /etc/apt/sources.list.d/nijel-ubuntu-phpmyadmin-xenial.list
         fi
         if [[ -f "/etc/apt/sources.list.d/nijel-ubuntu-phpmyadmin-xenial.list.save" ]]; then
             rm /etc/apt/sources.list.d/nijel-ubuntu-phpmyadmin-xenial.list.save
         fi
-        echo ""
     else
         info "- Nothing to do!"
     fi
