6b55815215775c9abc10ceed09335734265b0915
MagicalCodeMonkey <mwoodru@gmail.com>
2019-08-21 23:50:29 -0400

    Reboot upon first complete run; fix some cleanup

--- .scripts/precheck_upgrade.sh
+++ .scripts/precheck_upgrade.sh
@@ -17,9 +17,6 @@ precheck_upgrade()
         run_script 'fixes_mono'
         info "Cleaning up some things..."
         rm "/etc/sudoers.d/firstrun"
-        sed -i 's#echo "Running precheck script"##g' "${DETECTED_HOMEDIR}/.bashrc"
-        sed -i 's#bash precheck.sh##g' "${DETECTED_HOMEDIR}/.bashrc"
-        sed -i 's#bash -c "$(curl -fsSL https://raw.githubusercontent.com/openflixr/Docs/.*/precheck.sh)"##g' "${DETECTED_HOMEDIR}/.bashrc"
         sed -i 's/.*#firstrun-startup//g' "${DETECTED_HOMEDIR}/.bashrc"
         run_script 'set_config' "PRECHECK_CLEANUP" "COMPLETED"
         info "|------------------------------------------------|"
@@ -33,19 +30,17 @@ precheck_upgrade()
             echo "echo '|------------------------------------------------|' #openflixr-ready" >> "${DETECTED_HOMEDIR}/.profile"
             echo "sed -i 's/.*#openflixr-ready//g' '${DETECTED_HOMEDIR}/.profile' #openflixr-ready" >> "${DETECTED_HOMEDIR}/.profile"
             info "- Done"
+            reboot
         fi
     elif [[ ${config[PRECHECK_UPGRADE]:-} == "COMPLETED" && ${config[PRECHECK_CLEANUP]:-} == "COMPLETED" ]]; then
         info "|------------------------------------------------|"
         info "| OpenFLIXR should now be ready for use!!        |"
         info "|------------------------------------------------|"
-        if [[ $(grep -c "#openflixr-ready" "${DETECTED_HOMEDIR}/.profile") == 0 ]]; then
-            info "Adding OpenFLIXR Ready Banner to .profile"
-            echo "" >> "${DETECTED_HOMEDIR}/.profile"
-            echo "echo '|------------------------------------------------|' #openflixr-ready" >> "${DETECTED_HOMEDIR}/.profile"
-            echo "echo '| OpenFLIXR should now be ready for use!!        |' #openflixr-ready" >> "${DETECTED_HOMEDIR}/.profile"
-            echo "echo '|------------------------------------------------|' #openflixr-ready" >> "${DETECTED_HOMEDIR}/.profile"
-            echo "sed -i 's/.*#openflixr-ready//g' '${DETECTED_HOMEDIR}/.profile' #openflixr-ready" >> "${DETECTED_HOMEDIR}/.profile"
-            info "- Done"
+        if [[ $(grep -c "#openflixr-ready" "${DETECTED_HOMEDIR}/.profile") != 0 ]]; then
+            sed -i 's/.*#openflixr-ready//g' '${DETECTED_HOMEDIR}/.profile'
+        fi
+        if [[ $(grep -c "#firstrun-startup" "${DETECTED_HOMEDIR}/.profile") != 0 ]]; then
+            sed -i 's/.*#firstrun-startup//g' "${DETECTED_HOMEDIR}/.bashrc"
         fi
     else
         error "... Well, this is unexpected..."
