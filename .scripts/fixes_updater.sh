a3211e5f0148b23df8277d6b7e8b1285bd2760c4
MagicalCodeMonkey <mwoodru@gmail.com>
2019-09-14 09:59:04 -0400

    Clean-up

--- .scripts/fixes_updater.sh
+++ .scripts/fixes_updater.sh
@@ -7,7 +7,6 @@ fixes_updater()
     info "Checking post update script..."
 
     if [[ ${config[FIRSTRUN_UPGRADE]:-} == "COMPLETED" || ${config[SETUP_COMPLETED]} == "Y" ]] && [[ $(grep -c "### setupopenflixr fixes" "/opt/openflixr/userscript.sh") == 0 ]]; then
-        sed -i 's/.*#firstrun-startup//g' "${DETECTED_HOMEDIR}/.bashrc"
         if [[ $(grep -c "### Setup permissions fixes" "/opt/openflixr/userscript.sh") != 0 ]]; then
             info "- Removing setup permissions fixes from the post update script"
             sed -i 's/.*### Setup permissions fixes//g' "/opt/openflixr/userscript.sh"
@@ -19,15 +18,15 @@ fixes_updater()
         info "- Adding setupopenflixr fixes to post update script"
         echo "" >> "/opt/openflixr/userscript.sh"
         echo "### setupopenflixr fixes" >> "/opt/openflixr/userscript.sh"
-        echo "bash /opt/OpenFLIXR2.SetupScript/.scripts/userscript.sh # setupopenflixr fixes" >> "/opt/openflixr/userscript.sh"
+        echo 'bash /opt/OpenFLIXR2.SetupScript/.scripts/userscript.sh # setupopenflixr fixes' >> "/opt/openflixr/userscript.sh"
         echo "" >> "/opt/openflixr/userscript.sh"
         info "- Done"
     elif [[ $(grep -c "### Setup permissions fixes" "/opt/openflixr/userscript.sh") == 0 ]]; then
         info "- Adding setup permissions fixes to post update script"
         echo "" >> "/opt/openflixr/userscript.sh"
         echo "### Setup permissions fixes" >> "/opt/openflixr/userscript.sh"
-        echo "sudo chmod +x /usr/local/bin/setupopenflixr" >> "/opt/openflixr/userscript.sh"
-        echo "sudo chmod +x /usr/bin/setupopenflixr" >> "/opt/openflixr/userscript.sh"
+        echo 'sudo chmod +x /usr/local/bin/setupopenflixr' >> "/opt/openflixr/userscript.sh"
+        echo 'sudo chmod +x /usr/bin/setupopenflixr' >> "/opt/openflixr/userscript.sh"
         echo "### End Setup permissions fixes" >> "/opt/openflixr/userscript.sh"
         echo "" >> "/opt/openflixr/userscript.sh"
         info "- Done"
