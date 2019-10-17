d286b4ef86abe36f8bc66a65911bbec8193a2b29
MagicalCodeMonkey <mwoodru@gmail.com>
2019-10-16 22:35:26 -0400

    Fix warn to warning and txt file

--- .scripts/unmaintained.sh
+++ .scripts/unmaintained.sh
@@ -3,12 +3,12 @@ set -euo pipefail
 IFS=$'\n\t'
 
 unmaintained() {
-    warn "!!!! This script is no longer maintained !!!!"
+    warning "!!!! This script is no longer maintained !!!!"
     # Dialog to display
     whiptail \
         --backtitle ${OF_BACKTITLE} \
         --title "Script no longer maintained..." \
         --clear \
-        --msgbox "$(cat ${SCRIPTPATH}/.misc/project_dead.txt)" \
+        --msgbox "$(cat ${SCRIPTPATH}/.misc/unmaintained.txt)" \
         0 0
 }
