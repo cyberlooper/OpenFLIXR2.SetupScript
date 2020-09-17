306a2d76aaa52d46428da3a467ee9fe6dce27f8e
-Z3US- <jlooper2020@gmail.com>
2020-09-17 00:39:23 +0000

    ombi install fix

--- .scripts/ombi.sh
+++ .scripts/ombi.sh
@@ -3,30 +3,45 @@ set -euo pipefail
 IFS=$'\n\t'
 
 ombi() {
+    echo "ombi start"
     readonly ombidir="/opt/Ombi"
-
+    echo "ombi dir 1"
     if [[ ! -d "${ombidir}" ]]; then
+        echo "ombi dir 2"
         mkdir ${ombidir}
+        echo "ombi dir 3"
     fi
 
+    echo "ombi get release"
     # From https://github.com/linuxserver/docker-ombi/blob/master/Dockerfile#L22
     OMBI_RELEASE=$(curl -sX GET "https://api.github.com/repos/tidusjar/Ombi/releases/latest" | awk '/tag_name/{print $4;exit}' FS='[""]')
-
+    echo "ombi release: ${OMBI_RELEASE}"
     curl -o /tmp/ombi-src.tar.gz -L "https://github.com/tidusjar/Ombi/releases/download/${OMBI_RELEASE}/linux.tar.gz"
+    echo "ombi retrieved?"
 
     if [[ -f /tmp/ombi-src.tar.gz ]]; then
+        echo "ombi retrieved!"
         tar xzf /tmp/ombi-src.tar.gz -C "${ombidir}"
+        echo "ombi extracted!"
+
+        chmod +x "${ombidir}/Ombi"
+        echo "ombi executable!"
+
+        cd "${ombidir}"
+        echo "Should now be in ${ombidir}"
+
+        apt install -y libicu-dev libunwind8 libcurl4-openssl-dev
+        echo "Things installed. Running ombi..."
+        ./Ombi &
+        echo "Ssshhhh... sleeping"
+        sleep 60
+        echo "Murder..."
+        pkill Ombi
+        echo "Dead"
+        cd "${STORE_PATH}"
+        echo "Should now be in ${STORE_PATH}"
     else
-        info "Failed to retrieve or extra Ombi"
+        error "Failed to retrieve or extra Ombi"
     fi
-
-    chmod +x "${ombidir}/Ombi"
-
-    cd ${ombidir}
-
-    apt install -y libicu-dev libunwind8 libcurl4-openssl-dev
-    ./Ombi &
-    sleep 60
-    pkill Ombi
-    cd ${STORE_PATH}
+echo "ombi done"
 }
