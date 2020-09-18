f2d708e33d1c4224803bfae8151d627f4d34259a
-Z3US- <jlooper2020@gmail.com>
2020-09-18 14:47:53 +0000

    update

--- .scripts/ombi.sh
+++ .scripts/ombi.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 IFS=$'\n\t'
 
 ombi_check() {
-    [[ -f /etc/resolv.conf ]] && info "Ombi Installed." || run_script 'ombi'
+    [[ -f /opt/Ombi/complete ]] && info "Ombi Installed." || run_script 'ombi'
 }
 
 ombi() {
